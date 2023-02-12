e6f5e136787c8c1aca827d4ef3ef38da0e1a71a3
Satkirti Sharma <98018041+satkirtisharma@users.noreply.github.com>
2023-02-12 22:00:48 +0530

    Create Fitness.java

---
+++ Fitness.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        for(int i=0;i<x;i++){
+            int a=sc.nextInt();
+            System.out.println(a*10);
+        }
+	}
+}
