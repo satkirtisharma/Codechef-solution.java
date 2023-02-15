fffd1f97e6cec1173d67ac2f45cb8545928933aa
Satkirti Sharma <98018041+satkirtisharma@users.noreply.github.com>
2023-02-15 16:50:11 +0530

    Create Bull_or_Bear.java

---
+++ Bull_or_Bear.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		for (int i=0;i<n;i++){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    if(x<y){
+		        System.out.println("PROFIT");   
+		    }
+		       else if(x>y){
+		            System.out.println("LOSS");
+		        }
+		        else{
+		            System.out.println("NEUTRAL");
+		        }
+		}
+	}
+}
