ac908801a6a89bf68daab33fd112de18b93f1818
Satkirti Sharma <98018041+satkirtisharma@users.noreply.github.com>
2023-02-17 10:23:53 +0530

    Create Sum_it.java

---
+++ Sum_it.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		for (int i=0;i<n;i++){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int a=sc.nextInt();
+		    if((x+y)==a){
+		        System.out.println("YES");
+		    }
+		    else System.out.println("NO");
+		}
+	}
+}
