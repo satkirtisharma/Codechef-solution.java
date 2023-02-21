6d52cff43f4720bb5c6dac65dc64aa303a17a163
Satkirti Sharma <98018041+satkirtisharma@users.noreply.github.com>
2023-02-21 15:21:58 +0530

    Create Tasty_Decisions.java

---
+++ Tasty_Decisions.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(x*2>y*5){
+                System.out.println("Chocolate");
+            }
+            else if(x*2<y*5){
+                System.out.println("Candy");
+            }
+            else{
+                System.out.println("Either");
+            }
+        }
+	}
+}
